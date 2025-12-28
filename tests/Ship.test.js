a4663888da972368d030eb04461482c1842f064c
Raul <developer.rbs@gmail.com>
2025-12-28 23:07:03 +0200

    Test: Add ship hit test and refactor tests with beforeEach

--- tests/Ship.test.js
+++ tests/Ship.test.js
@@ -1,6 +1,16 @@
 import { Ship } from "../src/Ship.js";
 
-test('create a ship with a defined length', () => {
-    const ship = new Ship(3);
+let ship;
+
+beforeEach(()=> {
+    ship = new Ship(3)
+})
+
+test('Create a ship with a defined length', () => {
     expect(ship.length).toBe(3);
+})
+
+test("Test if ship can get hit", () => {
+    ship.hit();
+    expect(ship.hits).toBe(1);
 })
