ae5f739fa07db8cc6da75cb168e0c1333b01f5de
Raul <developer.rbs@gmail.com>
2025-12-30 00:49:47 +0200

    Feat: add support for placing ships longer than one cell

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -2,10 +2,25 @@ export function Gameboard () {
     let grid = Array(10).fill(null).map(() => Array(10).fill(null));
     
     const placeShip = (ship, [row, col]) => {
-        grid[row][col] = {
+
+        const cellStateObject = {
             occupied: true,
             shipReference: ship,
         }
+
+        const totalCellOccupied = ship.length
+
+        /*
+            we need to grab the length of the ship
+            we have to loop starting from the firs col 
+            for each cell we need to set it equal to the cellStateObject
+
+        */
+
+        for(let i = col; i < col+totalCellOccupied; i++){
+            grid[row][i] = cellStateObject;
+        }
+
     }
     
     return { grid, placeShip }
