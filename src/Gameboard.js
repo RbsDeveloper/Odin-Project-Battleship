14e4a7e750f85fb59705ec1463cd79a9fd43de03
Raul <developer.rbs@gmail.com>
2025-12-30 00:31:53 +0200

    feat: implement basic placeShip method on Gameboard

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,4 +1,12 @@
 export function Gameboard () {
     let grid = Array(10).fill(null).map(() => Array(10).fill(null));
-    return { grid }
+    
+    const placeShip = (ship, [row, col]) => {
+        grid[row][col] = {
+            occupied: true,
+            shipReference: ship,
+        }
+    }
+    
+    return { grid, placeShip }
 }
