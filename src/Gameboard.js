ce31203f025488e51ab56aadd312435ce4f26e7a
Raul <developer.rbs@gmail.com>
2026-01-03 15:11:30 +0200

    Feat: add direction feature

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,37 +1,49 @@
 export function Gameboard () {
     let grid = Array(10).fill(null).map(() => Array(10).fill(null));
     
-    const placeShip = (ship, [row, col]) => {
+    const placeShip = (ship, direction = "vertical", [row, col]) => {
 
         const cellStateObject = {
             occupied: true,
             shipReference: ship,
         }
 
+        const cellType = direction === "vertical" ? row : col;
         const totalCellOccupied = ship.length
-        const lastCellOfShip = col + (totalCellOccupied - 1);
+        const lastCellOfShip = cellType + (totalCellOccupied - 1);
 
-        if( lastCellOfShip > 9){
-            throw new Error("Ship can't be placed out of the grid")
-        }else{
-
-            let areCellsFree = true;
+        if( lastCellOfShip > 9) throw new Error("Ship can't be placed out of the grid")
+        
+        if(direction === 'vertical'){
+            for(let i = cellType; i < cellType + totalCellOccupied; i++){
             
-            for(let i = col; i < col+totalCellOccupied; i++){
-                if(grid[row][i] !== null){
-                    areCellsFree = false;
+                if(grid[i][col] !== null){
                     throw new Error("Ship can't be placed: overlapping ship")
                 }
             }
-
-            
-            for(let i = col; i < col+totalCellOccupied; i++){
-                grid[row][i] = cellStateObject;
-            }   
-            
+        }else{
+            for(let i = cellType; i < cellType + totalCellOccupied; i++){
+                if(grid[row][i] !== null){
+                    throw new Error("Ship can't be placed: overlapping ship")
+                }
+            }
         }
-
+         
+        if(direction === 'vertical'){
+            for(let i = cellType; i < cellType + totalCellOccupied; i++){
+                grid[i][col] = cellStateObject;
+            }
+            
+        }else{
+            for(let i = cellType; i < cellType + totalCellOccupied; i++){
+                grid[row][i] = cellStateObject;
+            }
+        }
+      
     }
     
     return { grid, placeShip }
 }
+
+
+
