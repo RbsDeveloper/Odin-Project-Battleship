34af34cb5972f470799a9d72dbe4f00a065f06a4
RbsDeveloper <developer.rbs@gmail.com>
2026-02-17 00:39:55 +0200

    Implement drag-and-drop event listeners to handle ship selection, real-time ghost previews, and final placement

--- src/events.js
+++ src/events.js
@@ -1,7 +1,8 @@
 import { gameState, resetEntireGameState, resetGameStateForReplay } from "./gameState.js";
 import { fireActionBasedOnBtnTarget, initGame, initializePlacementUI, pvpRound, runRound, triggerPhase } from "./gameController.js";
-import { isPlacementCompleted, selectShip, placeRandomFleet, attemptShipPlacement } from "./placementController.js";
-import { clearPlacementComponents, clearWindow, disableConfirmBtn, enableConfirmBtn, renderGameScreen } from "./ui.js";
+import { isPlacementCompleted, selectShip, placeRandomFleet, attemptShipPlacement, getActiveShipFromPlayerFleet } from "./placementController.js";
+import { clearPlacementComponents, clearWindow, disableConfirmBtn, enableConfirmBtn, highlightPlacement, renderGameScreen, resetHighlightPlacement } from "./ui.js";
+import { getGhostCoords, getValidPlacementCoords } from "./Gameboard.js";
 
 export function attachStartBtnLister (element) {
     
@@ -108,5 +109,66 @@ export function attachEventForPlayAgainBtn (element) {
         clearWindow();
         resetGameStateForReplay();
         triggerPhase("placement")
+    })
+}
+
+export function attachDragEvent (element) {
+    element.addEventListener('dragstart', (event) => {
+
+        const draggedElement = event.target
+
+        if(draggedElement.classList.contains("ship")){
+            console.log('dragging')
+            console.log(draggedElement);
+            selectShip(draggedElement.id)
+        }
+    })
+}
+
+export function attachDragOverEvent (element) {
+    element.addEventListener('dragover', (event) => {
+        event.preventDefault();
+        const cell = event.target.closest(".cell");
+        if(!cell) return
+        const cellRow = parseInt(cell.getAttribute("data-row"));
+        const cellCol = parseInt(cell.getAttribute("data-col"));
+
+        //obtaining active ship : 
+        const player = gameState.players[gameState.currentPlayer];
+        const shipReference = getActiveShipFromPlayerFleet(player);
+        
+        resetHighlightPlacement(player.id);
+
+        const ghostCoords = getGhostCoords(shipReference, gameState.shipDirection, [cellRow, cellCol]);
+        const coords = getValidPlacementCoords(shipReference, gameState.shipDirection, [cellRow, cellCol], player.getBoard().grid)
+
+        if(coords){
+            highlightPlacement(player.id, coords, true);
+        }else{
+            highlightPlacement(player.id, ghostCoords, false);
+        }
+        
+    })
+}
+
+export function attachDragLeaveEvent (element) {
+    element.addEventListener('dragleave', () => {
+        const player = gameState.players[gameState.currentPlayer];
+        resetHighlightPlacement(player.id)
+    })
+}
+
+export function attachDropEvent (element) {
+    element.addEventListener("drop", (event)=> {
+        event.preventDefault()
+        const cell = event.target.closest(".cell");
+        
+        if(!cell) return
+        const cellRow = parseInt(cell.getAttribute("data-row"));
+        const cellCol = parseInt(cell.getAttribute("data-col"));
+        
+        resetHighlightPlacement(gameState.players[gameState.currentPlayer].id);
+        attemptShipPlacement(cellRow, cellCol);
+        if(isPlacementCompleted(gameState.players[gameState.currentPlayer])) enableConfirmBtn();
     })
 }
