930ba4fa4634ee227579eb321518747e86bb4c0a
RbsDeveloper <developer.rbs@gmail.com>
2026-02-20 14:46:18 +0200

    refactor: centralize placement phase transitions in gameController implement handler functions to manage event callbacks

--- src/gameController.js
+++ src/gameController.js
@@ -1,7 +1,7 @@
-import { startDialog, insertSettingsForm, renderGameScreen, createPlayerBoardsArea, buildShip, createShipPlacementUi, renderPlacementScreen, renderGameboard, markCellAsHit, renderWinnerDialog, updateGameMessage } from "./ui.js";
-import { attachActiveShipEventListener, attachBoardEventListener, attachFormEventListener, attachPlacementBtnsEventListener, attachStartBtnLister, proceedToSecondPlayerPlacement, enterGamePhaseForPvC, attachComputerBoardClicks, attachEventForPvpMatch, attachEventForNewGamebtn, attachEventForPlayAgainBtn, attachDragEvent, attachDragOverEvent, attachDropEvent, attachDragLeaveEvent } from "./events.js";
+import { startDialog, insertSettingsForm, renderGameScreen, createPlayerBoardsArea, buildShip, createShipPlacementUi, renderPlacementScreen, renderGameboard, markCellAsHit, renderWinnerDialog, updateGameMessage, enableConfirmBtn, disableConfirmBtn, resetHighlightPlacement, clearPlacementComponents } from "./ui.js";
+import { attachActiveShipEventListener, attachBoardEventListener, attachFormEventListener, attachPlacementBtnsEventListener, attachStartBtnLister, attachEventForNewGamebtn, attachEventForPlayAgainBtn, attachDragOverEvent, attachDropEvent, attachDragLeaveEvent, attachDragStartListener, attachConfirmBtnListener } from "./events.js";
 import { createPlayers, toggleSecondPlayerInput } from "./playerSetup.js";
-import { placeFleetRandomlyForCurrentPlayer, resetPlayerBoard, changeShipDirection } from "./placementController.js";
+import { placeFleetRandomlyForCurrentPlayer, resetPlayerBoard, changeShipDirection, attemptShipPlacement, isPlacementCompleted, selectShip, handlePlacementHover, handlePlacementDrop, placeRandomFleet } from "./placementController.js";
 import { gameState, getBoards } from "./gameState.js";
 import { delayActions, getRandomCoord, opponentIndex } from "./utils.js";
 import { playSound } from "./soundManager.js";
@@ -20,7 +20,13 @@ export function triggerPhase(phase) {
 export function initGame () {  
     triggerPhase("start");
     const startBtn = document.getElementById("sgBtn");
-    attachStartBtnLister(startBtn);
+    attachStartBtnLister(startBtn, handleStartClick);
+}
+
+function handleStartClick (event){
+    const button = event.target;
+    button.remove();
+    triggerPhase("settings")
 }
 
 function enterStartPhase () {
@@ -29,26 +35,48 @@ function enterStartPhase () {
     modal.show()
 }
 
+function handleSubmitClick (e) {
+    e.preventDefault();
+    const formElement = e.target
+    const formData = new FormData(formElement);
+    const modal = document.getElementById("startingWindow")
+    modal.close();
+    modal.remove();
+    gameState.settings = Object.fromEntries(formData);
+    triggerPhase("placement");
+}
+
 function enterSettingsPhase () {
     const modal = document.getElementById("startingWindow");
     const formElement = insertSettingsForm();
     modal.append(formElement);
     toggleSecondPlayerInput();
-    attachFormEventListener(formElement, modal);
+    attachFormEventListener(formElement, handleSubmitClick);
+}
+
+function handlePlacementConfirmation() {
+    if(gameState.settings.mode === "pvp"){
+        if(gameState.currentPlayer === 0){
+            gameState.currentPlayer = 1;
+            disableConfirmBtn()
+            clearPlacementComponents()
+            initializePlacementUI()
+        } else {
+            triggerPhase("game");
+        }
+    }else {
+        gameState.currentPlayer = 1;
+        placeRandomFleet();
+        triggerPhase("game")
+    }
 }
 
 function enterPlacementPhase () {
     createPlayers(gameState.settings);
     document.body.append(renderPlacementScreen());
     initializePlacementUI()
-    
     const confirmBtn = document.getElementById("confirmPlacementBtn");
-
-    if(gameState.settings.mode === "pvp"){
-        proceedToSecondPlayerPlacement(confirmBtn);
-    }else{
-        enterGamePhaseForPvC(confirmBtn)
-    }
+    attachConfirmBtnListener(confirmBtn, handlePlacementConfirmation)
 }
 
 function enterGamePhase () {
@@ -63,6 +91,19 @@ function enterGamePhase () {
         singlePlayerMatch()
     }else{
         pvpMatch()
+    }
+}
+
+export function handleBoardClick(targetEl) {
+    const row = parseInt(targetEl.dataset.row);
+    const col = parseInt(targetEl.dataset.col);
+    if(gameState.gamePhase === "placement"){
+        attemptShipPlacement(row, col);
+        if(isPlacementCompleted(gameState.players[gameState.currentPlayer])){
+            enableConfirmBtn()
+        }
+    }else if(gameState.gamePhase === "game"){
+        processAttack(targetEl)
     }
 }
 
@@ -96,158 +137,92 @@ export function fireActionBasedOnBtnTarget (targetBtnId) {
     }
 }
 
+function handleDragStart (elementId) {
+    selectShip(elementId);
+}
+
+function handleDragLeave () {
+    const player = gameState.players[gameState.currentPlayer];
+    resetHighlightPlacement(player.id)
+}
+
 function loadPlacementContainer () {
     const interactiveBoard = document.getElementById("placementArea");
     const playerBoards = getBoards();
     interactiveBoard.append(renderGameboard(playerBoards[gameState.currentPlayer]));
     const shipContainer = document.querySelector(".shipContainer");
-    attachActiveShipEventListener(shipContainer)
-    attachDragEvent(shipContainer);
+    attachActiveShipEventListener(shipContainer, handleDragStart)
+    attachDragStartListener(shipContainer, selectShip);
     const playerBoard = document.querySelector(`.board[data-player-id = '${gameState.players[gameState.currentPlayer].id}']`);
-    attachBoardEventListener(playerBoard);
-    attachDragOverEvent(playerBoard);
-    attachDragLeaveEvent(playerBoard)
-    attachDropEvent(playerBoard);
+    attachBoardEventListener(playerBoard, handleBoardClick);
+    attachDragOverEvent(playerBoard, handlePlacementHover);
+    attachDragLeaveEvent(playerBoard, handleDragLeave)
+    attachDropEvent(playerBoard, handlePlacementDrop);
     const btnsContainer = document.querySelector(`.btnContainer[data-player-id = '${gameState.players[gameState.currentPlayer].id}']`);
-    attachPlacementBtnsEventListener(btnsContainer);
+    attachPlacementBtnsEventListener(btnsContainer, fireActionBasedOnBtnTarget);
 }
 
 function singlePlayerMatch () {
     const opponentPlayer = gameState.players[opponentIndex(gameState.currentPlayer)];
     const computerBoard = document.querySelector(`.board[data-player-id = "${opponentPlayer.id}"]`);
-    attachComputerBoardClicks(computerBoard);
-}
-
-export async function runRound (eventData) {
-    
-    if(gameState.isProcessingTurn) return 
-
-    gameState.isProcessingTurn = true;
-
-    const row = parseInt(eventData.getAttribute("data-row"));
-    const col = parseInt(eventData.getAttribute("data-col"));
-    const opponentPlayer = gameState.players[opponentIndex(gameState.currentPlayer)];
-    
-    const hitOrNot = opponentPlayer.getBoard().receiveAttack([row, col]);
-    if(hitOrNot === null){
-        gameState.isProcessingTurn = false;
-        return
-    }
-
-    playSound("fire")
-    await delayActions(1000).then(()=>{
-        playSound(hitOrNot)
-        markCellAsHit(hitOrNot, eventData);
-        if(hitOrNot==='hit'){
-            updateGameMessage("Boom! You hit something!")
-        }else if(hitOrNot === 'miss') {
-            updateGameMessage("Darn, just water. Miss!")
-        }
-    })
-    
-    if(checkLoss(opponentPlayer)){
-        triggerPhase("winner");
-        gameState.isProcessingTurn = false;
-        return
-    }
-    await delayActions(1500);
-    updateGameMessage("Incoming fire! Computer is targeting...")
-
-    gameState.currentPlayer = opponentIndex(gameState.currentPlayer)
-    await computerAttack()  
-    updateGameMessage("Your turn! Take your next shot.");    
-    gameState.isProcessingTurn = false;
-   
-}
- 
-async function computerAttack () {
-    const opponentPlayer = gameState.players[opponentIndex(gameState.currentPlayer)];
-    const humanBoard = document.querySelector(`.board[data-player-id = "${opponentPlayer.id}"]`)
-    let hitOrNot = null
-
-    await delayActions(1000)
-
-    while (hitOrNot === null) {
-        const rowTarget = getRandomCoord();
-        const colTarget = getRandomCoord();
-
-        hitOrNot = opponentPlayer.getBoard().receiveAttack([rowTarget, colTarget]);
-
-        if(hitOrNot === null){ 
-            continue
-        }
-        playSound('fire')   
-        const targetCell = humanBoard.querySelector(`.cell[data-row = "${rowTarget}"][data-col = "${colTarget}"]`)
-       
-        await delayActions(1000).then(()=> {
-            playSound(hitOrNot)
-            markCellAsHit(hitOrNot, targetCell);
-        })
-    }
-    
-    if(checkLoss(opponentPlayer)){
-        triggerPhase("winner");
-        return
-    }
-    gameState.currentPlayer = opponentIndex(gameState.currentPlayer); 
+    attachBoardEventListener(computerBoard, handleBoardClick);
 }
 
 function pvpMatch () {
     const boardsContainer = document.getElementById("boardsArea");
-    attachEventForPvpMatch(boardsContainer);
+    attachBoardEventListener(boardsContainer, handleBoardClick);
 }
 
-export async function pvpRound (eventData) {
-    const activePlayer = gameState.players[gameState.currentPlayer];
-    const targetedBoard = eventData.parentElement
+async function computerAttack () {
+    const opponentPlayer = gameState.players[opponentIndex(gameState.currentPlayer)];
+    const humanBoard = document.querySelector(`.board[data-player-id = "${opponentPlayer.id}"]`)
+    const computerAttackCoords = gameState.players[gameState.currentPlayer].getNextMove();
+    console.log(computerAttackCoords)
 
-    if(gameState.isProcessingTurn) return
-    gameState.isProcessingTurn = true;
-    
-    if(targetedBoard.getAttribute("data-player-id") === activePlayer.id){
-        console.log('wrong board');
-        gameState.isProcessingTurn = false
-        return
-    }
+    await delayActions(1000)
 
-    const row = parseInt(eventData.getAttribute("data-row"));
-    const col = parseInt(eventData.getAttribute("data-col"));
-    const opponentPlayer = gameState.players[opponentIndex(gameState.currentPlayer)]
+        const resultOfTheAttack = opponentPlayer.getBoard().receiveAttack([computerAttackCoords.row, computerAttackCoords.col]);
 
-    const hitOrNot = opponentPlayer.getBoard().receiveAttack([row, col]);
-    console.log(hitOrNot)
-    if(hitOrNot === null){ 
-        console.log('hit the same cell') 
-        gameState.isProcessingTurn = false;   
-        return 
-    }
+       
+        playSound('fire')   
+        const targetCell = humanBoard.querySelector(`.cell[data-row = "${computerAttackCoords.row}"][data-col = "${computerAttackCoords.col}"]`)
+       
+        await delayActions(1000)
+        playSound(resultOfTheAttack)
+        markCellAsHit(resultOfTheAttack, targetCell);
 
-    playSound('fire');
-    
-    await delayActions(1000).then(() => {
-        playSound(hitOrNot);
-        markCellAsHit(hitOrNot, eventData);
-        if(hitOrNot==='hit'){
-            updateGameMessage(`Boom! ${activePlayer.id} scored a hit!`)
-        }else if(hitOrNot === 'miss') {
-            updateGameMessage(`Splash!${activePlayer.id} missed!`)
+        if(resultOfTheAttack==='hit'){
+            updateGameMessage(`Boom! ${gameState.players[gameState.currentPlayer].id} scored a hit!`)
+        }else if(resultOfTheAttack === 'miss') {
+            updateGameMessage(`Splash!${gameState.players[gameState.currentPlayer].id} missed!`)
         }
-    })
     
     
+    await delayActions(500)
     if(checkLoss(opponentPlayer)){
         triggerPhase("winner");
         gameState.isProcessingTurn = false
         return
     }
-    await delayActions(1500)
-    gameState.currentPlayer = opponentIndex(gameState.currentPlayer);
-    updateGameMessage(`It is now ${gameState.players[gameState.currentPlayer].id}'s turn! Target the enemy.`);
-    gameState.isProcessingTurn = false
+    await delayActions(1000);
+    updateGameMessage(`It is now ${opponentPlayer.id}'s turn!`);
+    gameState.currentPlayer = opponentIndex(gameState.currentPlayer); 
 }
 
 function checkLoss (playerToCheck) {
     return playerToCheck.getBoard().areAllShipSunk();   
+}
+
+function handleNewGame () {
+    clearWindow();
+    resetEntireGameState()
+    initGame()
+}
+
+function handlePlayAgain () {
+    clearWindow();
+    resetGameStateForReplay();
+    triggerPhase("placement")
 }
 
 function enterWinnerPhase () {
@@ -257,6 +232,63 @@ function enterWinnerPhase () {
     const newGameBtn = document.getElementById("startNewGame");
     const playAgainBtn =  document.getElementById("restartSamePlayers");
 
-    attachEventForNewGamebtn(newGameBtn);
-    attachEventForPlayAgainBtn(playAgainBtn);
+    attachEventForNewGamebtn(newGameBtn, handleNewGame);
+    attachEventForPlayAgainBtn(playAgainBtn, handlePlayAgain);
+}
+
+function processAttack(targetEl) {
+    const playerBoard = targetEl.parentElement;
+    const activePlayer = gameState.players[gameState.currentPlayer];
+    const opponentPlayer = gameState.players[opponentIndex(gameState.currentPlayer)];
+    const row = parseInt(targetEl.dataset.row);
+    const col = parseInt(targetEl.dataset.col);
+
+    //We check if we are already in the middle of an attack or not
+    if(gameState.isProcessingTurn)return;
+    gameState.isProcessingTurn = true;
+
+    //We check if the click happens on the wrong board(especially for pvp);
+    if(playerBoard.dataset.playerId === activePlayer.id){
+        console.log("wrong board clicked")//here to know what happens, alter will refactor
+        gameState.isProcessingTurn = false;
+        return
+    }
+    handleCombatFlow(targetEl,activePlayer, opponentPlayer, row, col )
+}
+
+async function handleCombatFlow(targetEl,currentPlayer , opponent, row, col) {
+
+    const resultOfTheAttack = opponent.getBoard().receiveAttack([row,col]);
+    if(resultOfTheAttack===null){
+        console.log("hit the same cell");
+        gameState.isProcessingTurn = false;
+        return
+    }
+
+    playSound('fire');
+    await delayActions(1000);
+    playSound(resultOfTheAttack);
+    markCellAsHit(resultOfTheAttack, targetEl);
+
+    if(resultOfTheAttack==='hit'){
+        updateGameMessage(`Boom! ${currentPlayer.id} scored a hit!`)
+    }else if(resultOfTheAttack === 'miss') {
+        updateGameMessage(`Splash!${currentPlayer.id} missed!`)
+    }
+    
+    await delayActions(500)
+    if(checkLoss(opponent)){
+        triggerPhase("winner");
+        gameState.isProcessingTurn = false
+        return
+    }
+
+    await delayActions(1000);
+    gameState.currentPlayer = opponentIndex(gameState.currentPlayer);
+    updateGameMessage(`It is now ${gameState.players[gameState.currentPlayer].id}'s turn!`);
+    
+    if(gameState.settings.mode === "pvc"){
+        await computerAttack()
+    }
+    gameState.isProcessingTurn = false
 }
