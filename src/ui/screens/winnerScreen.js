9f856d302e15d0a89daae535811726f32c83e516
RbsDeveloper <developer.rbs@gmail.com>
2026-03-22 13:16:26 +0200

    fix/style: rework winner dialog structure, styling and content

--- src/ui/screens/winnerScreen.js
+++ src/ui/screens/winnerScreen.js
@@ -1,14 +1,17 @@
 import {createCompleteElement} from "../uiUtils/domHelpers.js"
 
 export function renderWinnerDialog (winnerPlayer) {
-    const winnerDialog = createCompleteElement("dialog", ["winnerDialog"], `The winner is: ${winnerPlayer}`, {id: "winnerDialog"});
-    const winnerParagraph = createCompleteElement("p", ["winnerMessage"], "", {id:"winnerMessage"});
+    const winnerDialog = createCompleteElement("dialog", ["winnerDialog"], "", {id: "winnerDialog"});
+    const dialogTitle =  createCompleteElement("h2", ["endGameTitle"], "battle concluded!")
+    const winnerParagraph = createCompleteElement("p", ["winnerMessage"], "The winner is:", {id:"winnerMessage"});
+    const winnerIdSpan = createCompleteElement("span", ["winnerSpanId"], `[${winnerPlayer}]`);
+    winnerParagraph.append(winnerIdSpan);
     const actions = createCompleteElement("div", ["restartBtnsContainer"], "");
-    const samePlayersRestart = createCompleteElement("button", ["btn", "samePlayersRestart"], "Play Again(same players)", {id: "restartSamePlayers"});
-    const newGame = createCompleteElement("button", ["btn", "newGameBtn"], "Start New Game", {id:"startNewGame"});
+    const samePlayersRestart = createCompleteElement("button", ["btn", "samePlayersRestart"], "Play Again", {id: "restartSamePlayers"});
+    const newGame = createCompleteElement("button", ["btn", "newGameBtn"], "New Game", {id:"startNewGame"});
 
     actions.append(newGame, samePlayersRestart);
-    winnerDialog.append(winnerParagraph, actions);
+    winnerDialog.append(dialogTitle, winnerParagraph, actions);
 
     return winnerDialog;
 }
