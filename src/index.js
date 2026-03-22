2739a48c0855894cbdaa5c2612069cb73eb0e052
RbsDeveloper <developer.rbs@gmail.com>
2026-03-22 22:52:45 +0200

    chore: add webpack config

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import '../style.css';
 import { initGame } from "./gameController.js";
 
 initGame()
---
+++ webpack.config.cjs
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './template.html',
+        }),
+    ],
+    devServer: {
+        static: [
+            { directory: path.resolve(__dirname, 'dist') },
+            { directory: path.resolve(__dirname, 'assets'), publicPath: '/assets' },
+        ],
+        open: true,
+    },
+    mode: 'development',
+};
